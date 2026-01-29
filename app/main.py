1ba78033cf80aeebe95db818885e83b141ae85fa
HugoTGabriel <hugp.gabrialves@gmail.com>
2026-01-29 15:45:15 -0300

    feat: adicionar interface HTML básica e integração com API de livros

--- app/main.py
+++ app/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from contextlib import asynccontextmanager
-
+from fastapi.middleware.cors import CORSMiddleware
 from app.core.database import create_db_and_tables
 from app.routes import books
 
@@ -12,6 +12,14 @@ async def lifespan(app: FastAPI):
 app = FastAPI(
     tittle='Bookstore API',
     lifespan=lifespan
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
 )
 
 app.include_router(books.router)
